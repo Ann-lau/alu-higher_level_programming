8b4417e7df9093cd56986f768ebae77513d26b9f
Ann Lau <a.lau@alustudent.com>
2022-07-07 15:28:27 -0700

    task 9

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        if ord(i) <= 123 and ord(i) >= 97:
-            upper = chr(ord(i) - 32)
-            i = upper
-            print("{}".format(i), end='')
-            print()
+offset = -32
+
+
+def uppercase(stru):
+    i = 0
+    while i < len(stru):
+        if islower(stru[i]):
+            holder = ord(stru[i]) + offset
+            new_str = chr(holder)
+        else:
+            new_str = stru[i]
+            print("{}".format(new_str), end='')
+            i = i + 1
+            print("")
+
+
+            def islower(strs):
+                value = ord(strs)
+                if value >= 97 and value <= 122:
+                    return True
+                else:
+                    return False
