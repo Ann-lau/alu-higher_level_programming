7faf45b0727b46fbb988db71324fccd4cc965225
Ann Lau <a.lau@alustudent.com>
2022-07-07 17:46:33 -0700

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+import sys
 
 
-def main()
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+def main():
+    args = sys.argv[1:]
+    args_len = len(args)
+    if args_len == 0:
+        print("0 arguments.")
+    elif args_len == 1:
+        print("1 argument:\n1: {}".format(args[0]))
+    else:
+        print("{:d} arguments:".format(args_len))
+        fir i in range(args_len):
+            print("{:d}: {}".format(i + 1, args[i]))
 
-if __name__ == '__main__':
-    main()
+            if __name__ == "__main__":
+                main()
