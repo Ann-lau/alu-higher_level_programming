9993e3682a57fd3ed49ef85e79eca7afb2acce14
Ann Lau <a.lau@alustudent.com>
2022-07-12 08:32:56 -0700

    task 1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Function that retrives an element from a list like in C
+def element_at(my_list, idx):
+    if (idx(-1)):
+        return
+    if idx > len(my_list):
+        return
+    return my_list[idx]
