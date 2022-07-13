9c748e322baa106a490e28bacb1727d971468d2e
root <root@ANNA.localdomain>
2022-07-13 12:27:27 +0200

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # Replaces an element of a list at a specific position
 def replace_in_list(my_list, idx, element):
-    if idx < 0 and idx > len[my_list]:
-        return [my_list, idx, element]
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        return my_list[idx] = element
