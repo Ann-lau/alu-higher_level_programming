0cd43d2e5f18c2c819c3db123ca119a8423faebe
Annedumo <dumoannad@gmail.com>
2022-07-13 16:29:41 +0200

    Replaces an element in alist at a specific position without modifying the original list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,13 +4,13 @@
 
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        c = my_list()
+        c = my_list[:]
         return c
     elif idx >= len(my_list):
-        d = my_list()
+        d = my_list[:]
         return d
     else:
-        v = my_list()
+        v = my_list[:]
         my_list[idx] = element
         return v
 new_in_list(my_list, idx, element)
