4874189d61042cb2df5386b9d92fe332b71b9475
Annedumo <dumoannad@gmail.com>
2022-07-15 22:53:48 +0200

    returns a new dictionary with all values multiplied by 2

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_Score(a_dictionary):
     if not a_dictionry:
-        retunrn None
+        return None
     max = list(a_dictionary.items())[0][1]
     for key value in a_dictionary.items():
         if value > max:
