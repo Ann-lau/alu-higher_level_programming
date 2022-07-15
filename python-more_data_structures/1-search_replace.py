2c21693342261d971836c942b34a85054d31007e
Annedumo <dumoannad@gmail.com>
2022-07-15 21:33:42 +0200

    replaces all occurrences of an element by another in a new list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    return (sum(set(my_list)))
+def search_replace(my_list, search, replace):
+    return [element if element != search else replace for element in my_list]
