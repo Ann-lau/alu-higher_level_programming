d8e62c816f37b9034b520e3ee6939a8710fae9bc
Annedumo <dumoannad@gmail.com>
2022-07-15 23:04:40 +0200

    returns a new dictionary with all values multiplied by 2

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionry:
-        return None
-    max = list(a_dictionary.items())[0][1]
-    for key value in a_dictionary.items():
-        if value > max:
-            max = value
-    for key, value in a_dictionary.items():
-        if value == max:
-            return key
+        """Returns a key with the biggest integer value."""
+            if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+               return None
+
+             ret = list(a_dictionary.keys())[0]
+             big = a_dictionary[ret]
+             for k, v in a_dictionary.items():
+                 if v > big:
+                    big = v
+                    ret = k
+             return (ret)
