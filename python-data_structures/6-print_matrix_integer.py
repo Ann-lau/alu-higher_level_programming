f67e3d26ac60cb068567df43ed4e52a822843d46
Annedumo <dumoannad@gmail.com>
2022-07-15 11:26:10 +0200

    prints a matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]): # prints a matrix of integers
+def print_matrix_integer(matrix=[[]]):
     for record in matrix:
         counter = 0
         for i in record:
             if counter == (len(record) -1):
                 print("{:d}".format(i), end="")
             else:
-                print("{:d}".format(i), end= " ")
+                print("{:d}".format(i), end=" ")
                 counter += 1
-         print("")
+                print("")
