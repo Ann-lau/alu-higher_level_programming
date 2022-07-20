50c98fbea042764b5bb8ca20b3252b34a8c7ab43
Annedumo <dumoannad@gmail.com>
2022-07-20 12:26:02 +0200

    divides 2 integers and prints the result

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
-
 def safe_print_division(a, b):
+    div = 0
     try:
-        a = a / b
-    except ZeroDivisionError:
-        a = None
+        div = a / b
+    except ZeroDivisionError
+        div = None
     finally:
-        print("Inside result:"< "{}".format(a))
-    return a
+        print("Inside result: {}".format(div))
+        return div
