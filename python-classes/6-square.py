464ec764a3fc97a8e8d79018d837c895255f7881
Annedumo <dumoannad@gmail.com>
2022-07-22 01:41:40 +0200

    class Square that defines a square by: (based on 6-square.py)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,43 +31,43 @@ class Square:
 
         self.__size = value
 
-        @property
-        def position(self):
+     @property
+     def position(self):
+         """
+         Sets and gets the value of private position attribute
             """
-            Sets and gets the value of private position attribute
-            """
-            return self.__position
+         return self.__position
 
-        @position.setter
-        def position(self, value):
-            if not isinstance(value, tuple):
+     @position.setter
+     def position(self, value):
+         if not isinstance(value, tuple):
                 raise TypeError("position must be a tuple of 2 positive integers")
-            if len(value) != 2:
+         if len(value) != 2:
                 raise TypeError("position must be a tuple of 2 positive integers")
-            if not (isinstance(value[0], int) and isinstance(value[1], int)):
+         if not (isinstance(value[0], int) and isinstance(value[1], int)):
                 raise TypeError("position must be a tuple of 2 positive integers")
-            if not (value[0] >= 0 and value[1] >= 0):
+         if not (value[0] >= 0 and value[1] >= 0):
                 raise TypeError("position must be a tuple of 2 positive integers")
 
-            self.__position = value
+         self.__position = value
 
-        def area(self):
-            """
-            Finds area of the square
-            """
-            return self.__size * self.__size
+     def area(self):
+         """
+         Finds area of the square
+         """
+         return self.__size * self.__size
 
-        def my_print(self):
-            """
-            Prints the square using # at correct position
-            """
-            if self.__size == 0:
-                print()
-                return
+    def my_print(self):
+         """
+         Prints the square using # at correct position
+         """
+         if self.__size == 0:
+             print()
+             return
 
-            # print self.__position[1] new lines
-            for i in range(self.__position[1]):
-                print()
+        # print self.__position[1] new lines
+        for i in range(self.__position[1]):
+            print()
 
-            for i in range(self.__size):
-                print(" " * self.__position[0] + '#' * self.__size)
+        for i in range(self.__size):
+            print(" " * self.__position[0] + '#' * self.__size)
