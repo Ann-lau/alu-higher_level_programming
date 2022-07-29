eae18dac51a854bff1e1e371cfeb35f6ed978af2
Annedumo <dumoannad@gmail.com>
2022-07-29 14:47:16 +0200

    Write a function that writes a string to a text file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file"""
+    with open('filename.txt', 'w') as f:
+        print(f.write('filename'))
