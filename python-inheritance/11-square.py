c35190a691a64e2d196be480ee8c4a01aed68b72
Annedumo <dumoannad@gmail.com>
2022-07-29 22:28:38 +0200

    class Square that inherits from Rectangle

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-base_geometry').Rectangle
+
+
+class Square(Rectangle):
+    """inherits from Rectangle"""
+
+    def __init__(self size):
+        """initializes data"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """returns [Square] <width>/<height>."""
+        return str("[Square] {}/{}".fomat(self.__size, self.__size))
+
+    def area(self):
+        """area of Square"""
+
+        return self.__size ** 2
