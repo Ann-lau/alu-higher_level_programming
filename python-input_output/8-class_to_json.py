567a82bc2db14ce4905b977bff4ccba3dd48f838
Annedumo <dumoannad@gmail.com>
2022-07-29 19:14:08 +0200

    function that returns the dictionary description with simple data structure

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+ function that returns the dictionary description
+ with simple data structure
+"""
+
+
+def class_to_json(obj):
+    """returns dictionary description"""
+    return obj.__dict__
