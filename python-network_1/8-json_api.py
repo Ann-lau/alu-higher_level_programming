78d430adbd223c0f1edb70969f6ebc2c7222690a
Annedumo <dumoannad@gmail.com>
2022-08-03 21:10:36 +0200

    search API

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
- script that takes in a letter and sends a POST request to
- http://0.0.0.0:5000/search_user with the letter as a parameter
- """
+"""search API"""
 import requests
 import sys
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     if len(sys.argv) > 1:
         arg = sys.argv[1]
     else:
@@ -16,7 +13,7 @@ if __name__ = '__main__':
         url = "http://0.0.0.0:5000/search_user"
         r = requests.post(url, data=payload)
         try:
-            r.raise_for_status
+            r.raise_for_status()
             json = r.json()
             if len(json) == 0:
                 print("No result")
