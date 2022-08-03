49a4914af4531f73512a3c838831aecd9c91cd6f
Annedumo <dumoannad@gmail.com>
2022-08-03 20:12:56 +0200

    posts an email

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""posts an email"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == '__main__':
+    values = {'email': sys.argv[2]}
+    data =  data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode('utf-8'))
