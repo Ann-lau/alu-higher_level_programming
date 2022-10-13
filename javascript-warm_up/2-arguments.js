2f5ba1a4133799367ab29df2b2b475281f0abe70
Ann-lau <a.lau@alustudent.com>
2022-10-13 03:36:40 -0700

    :robot: automated commit of one or many files

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@ const commands = require('process');
 const length = commands.argv.length;
 
 if (length > 3) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 } else if (length === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
